5896314baee07baf62aa59c184d27821231f9af0
hellosirandy <sac840711@gmail.com>
2017-06-11 14:56:39 +0800

    bind api to views

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import sys
+from chat_ass import ChatAid
+CA = ChatAid()
 
-# Create your views here.
+class Recommend(APIView):
+    def get(self, request, user_input):
+        result = CA.query_sent(user_input)
+        return Response({'result': result})
+    def post(self):
+        pass
